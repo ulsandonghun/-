37f70580c9c910e67c7793bd2c208b15db9883ae
ulsandonghun <donghoon141@naver.com>
2023-05-20 00:09:47 +0900

    [add] : ServerEventHandler 설정완료

--- src/main/java/CMServerEventHandler.java
+++ src/main/java/CMServerEventHandler.java
@@ -1,2 +1,36 @@
+import kr.ac.konkuk.ccslab.cm.event.CMEvent;
+import kr.ac.konkuk.ccslab.cm.event.CMSessionEvent;
+import kr.ac.konkuk.ccslab.cm.info.CMInfo;
+import kr.ac.konkuk.ccslab.cm.stub.CMServerStub;
+
 public class CMServerEventHandler {
+    private CMServerStub m_serverStub;
+    public CMServerEventHandler(CMServerStub serverStub)
+    {
+        m_serverStub = serverStub;
+    }
+    @Override
+    public void processEvent(CMEvent cme) {
+
+        switch(cme.getType())
+        {
+            case CMInfo.CM_SESSION_EVENT:
+                processSessionEvent(cme);
+                break;
+            default:
+                return;
+        }
+    }
+    private void processSessionEvent(CMEvent cme)
+    {
+        CMSessionEvent se = (CMSessionEvent) cme;
+        switch(se.getID())
+        {
+            case CMSessionEvent.LOGIN:
+                System.out.println("["+se.getUserName()+"] requests login.");
+                break;
+            default:
+                return;
+        }
+    }
 }
