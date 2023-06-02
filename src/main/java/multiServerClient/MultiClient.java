b7149afed91fabe0603b353b4e17a53658c2b184
ulsandonghun <donghoon141@naver.com>
2023-06-03 03:42:09 +0900

    [add] :클라이언트에게 파일전송 UI 추가

--- src/main/java/multiServerClient/MultiClient.java
+++ src/main/java/multiServerClient/MultiClient.java
@@ -23,6 +23,9 @@ public class MultiClient {
     private JButton checkButton;
     private JButton syncButton;
 
+    private JTextArea sendClientArea;
+    private JButton sendClientButton;
+
     private Socket socket;
     private BufferedReader in;
     private PrintStream out;
@@ -48,6 +51,7 @@ public class MultiClient {
             sendButton1.addActionListener(new SendButton1Listener());
             checkButton.addActionListener(new CheckButtonListener());
             syncButton.addActionListener(new syncbuttonListener());
+            sendClientButton.addActionListener(new sendClientListener());
             new ReceiveThread().start(); // Start a new thread to receive messages from the server
         } catch (IOException e) {
             e.printStackTrace();
@@ -109,6 +113,19 @@ public class MultiClient {
         frame.add(syncButton);
 
 
+        sendClientArea = new JTextArea();
+        sendClientArea.setBounds(20, 430, 200, 30);
+        frame.add(sendClientArea);
+
+
+
+        sendClientButton = new JButton("클라이언트에게 파일 전송");
+        sendClientButton.setBounds(240, 430, 240, 30);
+        frame.add(sendClientButton);
+
+
+
+
         frame.setVisible(true);
     }
 
@@ -147,25 +164,25 @@ public class MultiClient {
             try {
                 FileInputStream fin=new FileInputStream(clientRepositoryPath+fileName);
                 // Send the file name to the server
-                byte[] buffer = new byte[1024];        //바이트단위로 임시저장하는 버퍼를 생성합니다.
-                int len;                               //전송할 데이터의 길이를 측정하는 변수입니다.
-                int data=0;                            //전송횟수, 용량을 측정하는 변수입니다.
+                byte[] buffer = new byte[1024];
+                int len;
+                int data=0;
 
-                while((len = fin.read(buffer))>0){     //FileInputStream을 통해 파일에서 입력받은 데이터를 버퍼에 임시저장하고 그 길이를 측정합니다.
-                    data++;                        //데이터의 양을 측정합니다.
+                while((len = fin.read(buffer))>0){
+                    data++;
                 }
 
-                int datas = data;                      //아래 for문을 통해 data가 0이되기때문에 임시저장한다.
+                int datas = data;
 
                 fin.close();
-                fin = new FileInputStream(clientRepositoryPath+fileName);   //FileInputStream이 만료되었으니 새롭게 개통합니다.
-                                  //데이터 전송횟수를 서버에 전송하고,
+                fin = new FileInputStream(clientRepositoryPath+fileName);
+
 
                 len = 0;
 
-                for(;data>0;data--){                   //데이터를 읽어올 횟수만큼 FileInputStream에서 파일의 내용을 읽어옵니다.
-                    len = fin.read(buffer);        //FileInputStream을 통해 파일에서 입력받은 데이터를 버퍼에 임시저장하고 그 길이를 측정합니다.
-                    dataOutputStream.write(buffer,0,0);       //서버에게 파일의 정보(1kbyte만큼보내고, 그 길이를 보냅니다.
+                for(;data>0;data--){
+                    len = fin.read(buffer);
+                    dataOutputStream.write(buffer,0,0);
                 }
 
 
@@ -201,6 +218,21 @@ public class MultiClient {
                 // Send the file name to the server for update check
 
                 out.println( "sync"+fileName);
+                out.flush();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    private class sendClientListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String fileName = sendClientArea.getText();
+            try {
+                // Send the file name to the server for update check
+
+                out.println( "send"+fileName);
                 out.flush();
             } catch (Exception ex) {
                 ex.printStackTrace();
